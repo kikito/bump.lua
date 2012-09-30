891371905cf14b6b9e9dc186a638cb47b487e2d3
kikito <kikito@gmail.com>
2012-09-30 20:05:25 +0200

    basic nodes specs work

--- bump/nodes.lua
+++ bump/nodes.lua
@@ -1,13 +1,29 @@
-local nodes = {}
-
-
+local nodes = {} -- (public/exported) holds the public methods of this module
+local store      -- (private) holds the list of created nodes
 
 function nodes.create(item)
-  nodes[item] = {}
+  assert(item, "item expected, got nil")
+  store[item] = {}
 end
 
 function nodes.get(item)
-  return nodes[item]
+  return store[item]
 end
+
+function nodes.reset()
+  store = setmetatable({}, {__mode = "k"})
+end
+
+function nodes.count()
+  local count = 0
+  for _,_ in pairs(store) do count = count + 1 end
+  return count
+end
+
+function nodes.destroy(item)
+  store[item] = nil
+end
+
+nodes.reset()
 
 return nodes
--- spec/nodes_spec.lua
+++ spec/nodes_spec.lua
@@ -2,11 +2,13 @@ local nodes = require 'bump.nodes'
 
 describe("bump.nodes", function()
 
+
   it("is a table", function()
     assert.equals(type(nodes), "table")
   end)
 
   describe(".get", function()
+    before_each(nodes.reset)
 
     it("returns nil for unknown items", function()
       assert.equals(nil, nodes.get({}))
@@ -16,6 +18,50 @@ describe("bump.nodes", function()
       local item = {}
       nodes.create(item)
       assert.equals("table", type(nodes.get(item)))
+    end)
+
+  end)
+
+  describe(".create", function()
+    before_each(nodes.reset)
+
+    it("throws an error when passed nil", function()
+      assert.error(function() nodes.create(nil) end)
+    end)
+
+    it("inserts new nodes in the list of nodes, but they get automatically gc", function()
+      assert.equals(0, nodes.count())
+      local item={}
+      nodes.create(item)
+      assert.equals(1, nodes.count())
+      item = nil
+      collectgarbage('collect')
+      assert.equals(0, nodes.count())
+    end)
+
+  end)
+
+  describe(".count", function()
+    before_each(nodes.reset)
+
+    it("returns the number of nodes available in the node store", function()
+      assert.equals(0, nodes.count())
+      nodes.create({})
+      assert.equals(1, nodes.count())
+    end)
+
+  end)
+
+  describe(".remove", function()
+    before_each(nodes.reset)
+
+    it("destroys a node given its corresponding item", function()
+      local item = {}
+      nodes.create(item)
+      assert.equals(1, nodes.count())
+      nodes.destroy(item)
+      assert.equals(nil, nodes.get(item))
+      assert.equals(0, nodes.count())
     end)
 
   end)
