ab6d225af7e6739ccdef280c047218df4d76c1d9
kikito <kikito@gmail.com>
2012-11-14 09:52:25 +0100

    updated bump_debug.lua

--- lib/bump_debug.lua
+++ lib/bump_debug.lua
@@ -1,24 +1,29 @@
-
-local bump = require 'lib.bump'
+local bump = require 'bump'
 
 local bump_debug = {}
 
--- transform grid coords into world coords
 local function _getCellBoundingBox(x,y)
   local cellSize = bump.getCellSize()
   return (x - 1)*cellSize, (y-1)*cellSize, cellSize, cellSize
 end
 
-local function _drawCell(cell, gx, gy)
-  local l,t,w,h   = _getCellBoundingBox(gx, gy)
-  local intensity = cell.itemCount * 40 + 30
+local function _countItems(cell)
+  local count = 0
+  for _,_ in pairs(cell.items) do count = count + 1 end
+  return count
+end
+
+local function _drawCell(cell)
+  local l,t,w,h   = _getCellBoundingBox(cell.gx, cell.gy)
+  local count = _countItems(cell)
+  local intensity = count * 40 + 30
   love.graphics.setColor(intensity, intensity, intensity)
-  love.graphics.print(cell.itemCount, l+12, t+12)
+  love.graphics.print(count, l+12, t+12)
   love.graphics.rectangle('line', l,t,w,h)
 end
 
 function bump_debug.draw(l,t,w,h)
-  bump.eachCell(_drawCell, l,t,w,h)
+  bump.cells.each(_drawCell, bump.geom.gridBox(bump.getCellSize(), l,t,w,h))
 end
 
 return bump_debug
