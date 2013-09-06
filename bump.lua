655d439e9ca35d1f75930ee5f9cd4f04e971f671
kikito <kikito@gmail.com>
2013-09-06 23:37:59 +0200

    initial bump and World implementations

--- bump.lua
+++ bump.lua
@@ -1,16 +1,35 @@
 local bump = {}
 
+local function checkType(desiredType, value, name)
+  if type(value) ~= desiredType then
+    error(name .. ' must be a ' .. desiredType .. ', but was ' .. tostring(value) .. '(a ' .. type(value) .. ')')
+  end
+end
 
-local function checkPositiveInteger(value, name)
+local function checkPositiveNumber(value, name)
   if type(value) ~= 'number' or value <= 0 then
     error(name .. ' must be a positive integer, but was ' .. tostring(value) .. '(' .. type(value) .. ')')
   end
 end
 
+local World = {}
+
+World.add = function(self, item, l,t,w,h)
+  checkType('table',  item, 'item')
+  checkType('number', l, 'l')
+  checkType('number', t, 'w')
+  checkType('number', w, 'w')
+  checkType('number', h, 'h')
+end
+
 bump.newWorld = function(cellSize)
   cellSize = cellSize or 64
-  checkPositiveInteger(cellSize, 'cellSize')
-  return { cellSize = cellSize }
+  checkPositiveNumber(cellSize, 'cellSize')
+  return setmetatable(
+    { cellSize = cellSize },
+    {__index = World }
+  )
 end
+
 
 return bump
--- spec/World_spec.lua
+++ spec/World_spec.lua
@@ -1,4 +1,14 @@
 local bump = require 'bump'
 
 describe('World', function()
+
+  describe(':add', function()
+    it('requires one table and 4 numbers', function()
+      local world = bump.newWorld()
+      assert.error(function() world:add({}) end)
+      assert.error(function() world:add({}, 40) end)
+      assert.error(function() world:add() end)
+    end)
+
+  end)
 end)
