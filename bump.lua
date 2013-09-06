574c034cf09f1ef5c28973b9cf79eb6e4727fb8b
kikito <kikito@gmail.com>
2013-09-07 00:43:06 +0200

    implemented brute force algorithm

--- bump.lua
+++ bump.lua
@@ -1,5 +1,8 @@
 local bump = {}
 
+
+local abs = math.abs
+
 local function checkType(desiredType, value, name)
   if type(value) ~= desiredType then
     error(name .. ' must be a ' .. desiredType .. ', but was ' .. tostring(value) .. '(a ' .. type(value) .. ')')
@@ -14,19 +17,64 @@ end
 
 local World = {}
 
-World.add = function(self, item, l,t,w,h)
+local function box_overlap(l1,t1,w1,h1, l2,t2,w2,h2)
+  -- check if there is a collision
+  if l1 < l2+w2 and l1+w1 > l2 and t1 < t2+h2 and t1+h1 > t2 then
+
+    -- get the two centers
+    local c1x, c1y = l1 + w1 * .5, t1 + h1 * .5
+    local c2x, c2y = l2 + w2 * .5, t2 + h2 * .5
+
+    -- get the two overlaps
+    local dx = l2 - l1 + (c1x < c2x and -w1 or w2)
+    local dy = t2 - t1 + (c1y < c2y and -h1 or h2)
+
+    return dx, dy
+  end
+  -- no collision; return nil
+end
+
+function World:add(item, l,t,w,h)
   checkType('table',  item, 'item')
   checkType('number', l, 'l')
   checkType('number', t, 'w')
   checkType('number', w, 'w')
   checkType('number', h, 'h')
+
+  self.items[item] = nil
+
+  local collisions, length = self:check(item, l,t,w,h)
+
+  self.items[item] = {l=l,t=t,w=w,h=h}
+  return collisions, length
+end
+
+function World:check(item, l,t,w,h)
+  local collisions, length = {}, 0
+
+  for other, hisBox in pairs(self.items) do
+    local dx, dy = box_overlap(l,t,w,h, hisBox.l, hisBox.t, hisBox.w, hisBox.h)
+    if dx then
+      length = length + 1
+      collisions[length] = {
+        self   = item,
+        other  = other,
+        dx     = dx,
+        dy     = dy
+      }
+    end
+  end
+
+  return collisions, length
 end
 
 bump.newWorld = function(cellSize)
   cellSize = cellSize or 64
   checkPositiveNumber(cellSize, 'cellSize')
   return setmetatable(
-    { cellSize = cellSize },
+    { cellSize = cellSize,
+      items = {}
+    },
     {__index = World }
   )
 end
--- spec/World_spec.lua
+++ spec/World_spec.lua
@@ -10,5 +10,32 @@ describe('World', function()
       assert.error(function() world:add() end)
     end)
 
+    describe('when the world is empty', function()
+      it('returns an empty list of collisions', function()
+        local world = bump.newWorld()
+        assert.same(world:add({}, 0,0,10,10), {})
+      end)
+    end)
+
+    describe('when the world is not empty', function()
+      it('returns an empty list of collisions', function()
+        local world, a, b = bump.newWorld(), {}, {}
+
+        world:add(a, 0,0,10,10)
+        assert.same(world:add(b, 4,6,10,10), {
+          { self = b, other = a, dx = 6, dy = 4 }
+        })
+
+      end)
+    end)
+  end)
+
+  describe(':check', function()
+    describe('when the item does not exist', function()
+      it('returns an empty table', function()
+        local world = bump.newWorld()
+        assert.same(world:check({}, 1,2,3,4), {})
+      end)
+    end)
   end)
 end)
