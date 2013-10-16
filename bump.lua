5ab67c61dc1a88be08591804afd4832485f6dde7
kikito <kikito@gmail.com>
2013-10-16 23:18:57 +0200

    undo unnecessary refactoring

--- bump.lua
+++ bump.lua
@@ -93,19 +93,11 @@ local function collideBoxes(l1,t1,w1,h1, l2,t2,w2,h2, vx,vy)
   end
 end
 
-local function getOrCreateRow(self, cy)
-  self.rows[cy] = self.rows[cy] or setmetatable({}, {__mode = 'v'})
-  return self.rows[cy]
-end
-
-local function getOrCreateCell(row, cx)
-  row[cx] = row[cx] or {itemCount = 0, items = setmetatable({}, {__mode = 'k'})}
-  return row[cx]
-end
-
 local function addItemToCell(self, item, cx, cy)
-  local row = getOrCreateRow(self, cy)
-  local cell = getOrCreateCell(row, cx)
+  self.rows[cy] = self.rows[cy] or setmetatable({}, {__mode = 'v'})
+  local row = self.rows[cy]
+  row[cx] = row[cx] or {itemCount = 0, items = setmetatable({}, {__mode = 'k'})}
+  local cell = row[cx]
   self.nonEmptyCells[cell] = true
   if not cell.items[item] then
     cell.items[item] = true
