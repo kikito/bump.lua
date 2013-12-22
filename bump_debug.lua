e24f3d5f3d0287504e65fb63dbf142a48f152efd
kikito <kikito@gmail.com>
2013-12-22 03:18:23 +0100

    Adapt bump_debug to the removal of world:toWorldBox

--- bump_debug.lua
+++ bump_debug.lua
@@ -3,10 +3,15 @@ local bump = require 'bump'
 
 local bump_debug = {}
 
+local function getCellRect(world, cx,cy)
+  local l,t = world:toWorld(cx,cy)
+  return l,t,world.cellSize,world.cellSize
+end
+
 function bump_debug.draw(world)
   for cy, row in pairs(world.rows) do
     for cx, cell in pairs(row) do
-      local l,t,w,h = world:toWorldBox(cell.x, cell.y, 1, 1)
+      local l,t,w,h = getCellRect(world, cx,cy)
       local intensity = cell.itemCount * 40 + 30
       love.graphics.setColor(intensity, intensity, intensity)
       love.graphics.print(cell.itemCount, l+12, t+12)
