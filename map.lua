a08eb1eec507e755352baa30de2fb97492dc235b
kikito <kikito@gmail.com>
2013-12-31 00:20:58 +0100

    removed global functions from Map

--- map.lua
+++ map.lua
@@ -6,14 +6,6 @@ local random = math.random
 
 local Map = class('Map')
 
-function newBlock(self, l,t,w,h)
-  Block:new(self.world, l,t,w,h)
-end
-
-function newCoin(self, l,t)
-  Coin:new(self.world, l,t)
-end
-
 Map.static.WIDTH  = 4000
 Map.static.HEIGHT = 2000
 
@@ -25,19 +17,19 @@ function Map:initialize(world)
   local width, height = self.width, self.height
 
   -- walls & ceiling
-  newBlock(self,        0,         0, width,        32)
-  newBlock(self,        0,        32,    32, height-64)
-  newBlock(self, width-32,        32,    32, height-64)
+  Block:new(world,        0,         0, width,        32)
+  Block:new(world,        0,        32,    32, height-64)
+  Block:new(world, width-32,        32,    32, height-64)
 
   -- tiled floor
   local tilesOnFloor = 40
   for i=0,tilesOnFloor - 1 do
-    newBlock(self, i*width/tilesOnFloor, height-32, width/tilesOnFloor, 32)
+    Block:new(world, i*width/tilesOnFloor, height-32, width/tilesOnFloor, 32)
   end
 
   -- random blocks
   for i=1,100 do
-    newBlock(self,
+    Block:new(world,
              random(100, width-200),
              random(100, height-150),
              random(32, 100),
@@ -46,7 +38,7 @@ function Map:initialize(world)
 
   -- random coins
   for i=1,20 do
-    newCoin(self, random(100, width-200), random(100, height-200))
+    Coin:new(world, random(100, width-200), random(100, height-200))
   end
 
 end
