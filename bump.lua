9283742f25f13b58d436b336b80ee8522b42fd17
kikito <kikito@gmail.com>
2014-02-12 20:35:07 +0100

    Collision:resolve returns true/nil

--- bump.lua
+++ bump.lua
@@ -254,7 +254,7 @@ function Collision:resolve()
     local px, py = minAbs(l, l+w), minAbs(t, t+h)     -- nearest point to 0,0
     local wi, hi = min(w1, abs(px)), min(h1, abs(py)) -- area of intersection
     self.ti = -wi * hi -- ti is the negative area of intersection
-    return self.kind
+    return true
   else
     local ti,_,nx,ny = getLiangBarskyIndices(l,t,w,h, 0,0,vx,vy)
     -- b1 tunnels into b2 while it travels
@@ -264,10 +264,9 @@ function Collision:resolve()
       self.ti   = ti
       self.normal_x = nx
       self.normal_y = ny
-      return self.kind
+      return true
     end
   end
-  return nil
 end
 
 ------------------------------------------------------------
