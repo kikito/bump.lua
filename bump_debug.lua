e703a1791f3303b914875b0c62598466cf1bc885
kikito <kikito@gmail.com>
2014-05-15 21:34:04 +0200

    make bump_debug a bit more visual

--- bump_debug.lua
+++ bump_debug.lua
@@ -12,8 +12,10 @@ function bump_debug.draw(world)
   for cy, row in pairs(world.rows) do
     for cx, cell in pairs(row) do
       local l,t,w,h = getCellRect(world, cx,cy)
-      local intensity = cell.itemCount * 40 + 30
-      love.graphics.setColor(intensity, intensity, intensity)
+      local intensity = cell.itemCount * 20 + 30
+      love.graphics.setColor(200,200,200, intensity)
+      love.graphics.rectangle('fill', l,t,w,h)
+      love.graphics.setColor(200,200,200)
       love.graphics.print(cell.itemCount, l+12, t+12)
       love.graphics.rectangle('line', l,t,w,h)
     end
