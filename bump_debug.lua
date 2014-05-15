607a49e167c359fb4b853772fd7af6a3557af3c2
kikito <kikito@gmail.com>
2014-05-15 21:52:43 +0200

    More tweaks to debugging graphics

--- bump_debug.lua
+++ bump_debug.lua
@@ -4,19 +4,26 @@ local bump = require 'bump'
 local bump_debug = {}
 
 local function getCellRect(world, cx,cy)
+  local cellSize = world.cellSize
   local l,t = world:toWorld(cx,cy)
-  return l,t,world.cellSize,world.cellSize
+  return l,t,cellSize,cellSize
 end
 
 function bump_debug.draw(world)
+  local cellSize = world.cellSize
+  local font = love.graphics.getFont()
+  local fontHeight = font:getHeight()
+  local topOffset = (cellSize - fontHeight) / 2
   for cy, row in pairs(world.rows) do
     for cx, cell in pairs(row) do
       local l,t,w,h = getCellRect(world, cx,cy)
-      local intensity = cell.itemCount * 16 + 16
+      local intensity = cell.itemCount * 12 + 16
       love.graphics.setColor(255,255,255,intensity)
       love.graphics.rectangle('fill', l,t,w,h)
-      love.graphics.setColor(255,255,255, 200)
-      love.graphics.print(cell.itemCount, l+12, t+12)
+      love.graphics.setColor(255,255,255, 64)
+      love.graphics.printf(cell.itemCount, l, t+topOffset, cellSize, 'center')
+      love.graphics.setColor(255,255,255,10)
+      love.graphics.rectangle('line', l,t,w,h)
     end
   end
 end
