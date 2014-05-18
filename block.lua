9b28cb81225c5e1129e7064885ebfb2db7d649e7
kikito <kikito@gmail.com>
2014-05-18 15:51:43 +0200

    mark border blocks as indestructible

--- block.lua
+++ block.lua
@@ -4,13 +4,17 @@ local util  = require 'util'
 
 local Block = class('Block')
 
-function Block:initialize(world, l,t,w,h)
+function Block:initialize(world, l,t,w,h, indestructible)
   self.world, self.l, self.t, self.w, self.h = world, l,t,w,h
+  self.indestructible = indestructible
   world:add(self, l,t,w,h)
 end
 
 function Block:draw()
   local r,g,b = 220, 150, 150
+  if self.indestructible then
+    r,g,b = 150,150,220
+  end
   util.drawFilledRectangle(self.l, self.t, self.w, self.h, r,g,b)
 end
 
@@ -19,6 +23,10 @@ function Block:getUpdateOrder()
 end
 
 function Block:update(dt)
+end
+
+function Block:destroy()
+  self.world:remove(self)
 end
 
 return Block
