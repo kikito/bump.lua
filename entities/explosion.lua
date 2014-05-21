e13527d191f9f451b6871e1f382efb23306b0379
kikito <kikito@gmail.com>
2014-05-21 22:53:04 +0200

    Explosions push things around

--- entities/explosion.lua
+++ entities/explosion.lua
@@ -9,10 +9,18 @@ Explosion.static.updateOrder = 0
 local width = 75
 local height = width
 
-local explosionFilter = function(other)
+local clamp = function(x, a, b)
+  return math.max(a, math.min(b, x))
+end
+
+local destroyFilter = function(other)
   return other.class.name == 'Guardian'
       or (other.class.name == 'Block'
           and not other.indestructible)
+end
+
+local pushFilter = function(other)
+  return other.class.name == 'Player' or other.class.name == 'Grenade'
 end
 
 function Explosion:initialize(world, x, y)
@@ -20,15 +28,38 @@ function Explosion:initialize(world, x, y)
 end
 
 function Explosion:draw()
-  local r,g,b = 255,255,0
+  local r,g,b = 255,0,0
   love.graphics.setColor(r,g,b)
   util.drawFilledRectangle(self.l, self.t, width, height, r,g,b)
 end
 
+function Explosion:push(other)
+  local cx, cy = self:getCenter()
+  local ox, oy = other:getCenter()
+  local dx, dy = ox - cx, oy - cy
+
+  dx, dy = clamp(dx, -300, 300),
+           clamp(dy, -300, 300)
+  if     dx > 0 then dx = 300 - dx
+  elseif dx < 0 then dx = dx - 300
+  end
+  if     dy > 0 then dy = 300 - dy
+  elseif dy < 0 then dy = dy - 300
+  end
+
+  other.vx = other.vx + dx
+  other.vy = other.vy + dy
+end
+
 function Explosion:update()
-  local cols, len = self.world:check(self, nil, nil, explosionFilter)
+  local cols, len = self.world:check(self, nil, nil, destroyFilter)
   for i=1,len do
     cols[i].other:destroy()
+  end
+
+  local items, len = self.world:queryRect(self.l - 100, self.t - 100, self.w + 200, self.h + 200, pushFilter)
+  for i=1,len do
+    self:push(items[i])
   end
 
   for i=1, math.random(10,20) do
