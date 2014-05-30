c472902423425de141ad9a37345dc29acd11facf
kikito <kikito@gmail.com>
2014-05-30 23:03:14 +0200

    update things a bit outside the camera

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ local media      = require 'media'
 local Map        = require 'map'
 
 
+local updateRadius = 100 -- how "far away from the camera" things stop being updated
 local drawDebug   = false  -- draw bump's debug info, fps and memory
 local instructions = [[
   bump.lua demo
@@ -34,7 +35,10 @@ function love.update(dt)
   -- Note that we only update elements that are visible to the camera. This is optional
   -- replace the map:update(dt, camera:getVisible()) with the following line to update everything
   -- map:update(dt)
-  map:update(dt, camera:getVisible())
+  local l,t,w,h = camera:getVisible()
+  l,t,w,h = l - updateRadius, t - updateRadius, w + updateRadius * 2, h + updateRadius * 2
+
+  map:update(dt, l,t,w,h)
   camera:setPosition(map.player:getCenter())
   camera:update(dt)
 end
