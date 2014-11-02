cc96d3ab45efa8d3c48bc5ab3e7faae8307c1653
kikito <kikito@gmail.com>
2014-11-02 23:20:56 +0100

    define world in a before each. refactor using item = world:add(...)

--- spec/World_spec.lua
+++ spec/World_spec.lua
@@ -1,6 +1,11 @@
 local bump = require 'bump'
 
 describe('World', function()
+  local world
+
+  before_each(function()
+    world = bump.newWorld()
+  end)
 
   local collect = function(t, field_name)
     local res = {}
@@ -10,7 +15,6 @@ describe('World', function()
 
   describe(':add', function()
     it('requires something + 4 numbers', function()
-      local world = bump.newWorld()
       assert.error(function() world:add({}) end)
       assert.error(function() world:add({}, 40) end)
       assert.error(function() world:add() end)
@@ -18,21 +22,16 @@ describe('World', function()
 
     it('returns the added item', function()
       local item = {}
-      local world = bump.newWorld()
       assert.equals(item, world:add(item, 1,1,1,1))
     end)
 
     it('throws an error if the object was already added', function()
-      local world = bump.newWorld()
-      local obj = {}
-      world:add(obj, 0,0,10,10)
+      local obj = world:add({}, 0,0,10,10)
       assert.error(function() world:add(obj, 0,0,10,10) end)
     end)
 
     describe('when the world is empty', function()
       it('creates as many cells as needed to hold the item', function()
-        local world = bump.newWorld()
-
         world:add({}, 0,0,10,10) -- adss one cell
         assert.equal(world:countCells(), 1)
 
@@ -55,22 +54,19 @@ describe('World', function()
 
     describe('when the object is not there', function()
       it('throws an error', function()
-        local world = bump.newWorld()
         assert.is_error(function() world:move({}, 0,0) end)
       end)
     end)
 
     it('moves the object', function()
-      local world, a = bump.newWorld(), {}
-      world:add(a, 0,0,10,10)
+      local a = world:add({}, 0,0,10,10)
       world:move(a, 40,40, 20,20)
       assert.same(world:getRect(a), {x=40,y=40,w=20,h=20})
     end)
 
     describe('when no width or height is given', function()
       it('takes width and height from its previous value', function()
-        local world, a = bump.newWorld(), {'a'}
-        world:add(a, 0,0, 10,10)
+        local a = world:add({}, 0,0,10,10)
         world:move(a, 5,5)
         assert.same({x=5,y=5,w=10,h=10}, world:getRect(a))
       end)
@@ -78,9 +74,7 @@ describe('World', function()
 
     describe('when the object stays in the same group of cells', function()
       it('does not invoke remove and add', function()
-        local world, a = bump.newWorld(), {}
-
-        world:add(a, 0,0,10,10)
+        local a = world:add({}, 0,0,10,10)
 
         spy.on(world, 'remove')
         spy.on(world, 'add')
@@ -96,63 +90,59 @@ describe('World', function()
   describe(':check', function()
     describe('when the item does not exist', function()
       it('throws an error', function()
-        local world = bump.newWorld()
+
         assert.error(function() world:check({}) end)
       end)
     end)
     describe('when the world is empty', function()
       it('returns an empty list of collisions', function()
-        local world = bump.newWorld()
-        local obj = {}
-        world:add(obj, 1,2,3,4)
+
+        local obj = world:add({}, 1,2,3,4)
         assert.same(world:check(obj), {})
       end)
     end)
 
     describe('when the world is not empty', function()
       it('returns a list of collisions', function()
-        local world, a, b, c = bump.newWorld(), {'a'}, {'b'}, {'c'}
 
-        world:add(a, 0,0,10,10)
-        world:add(b, 4,6,10,10)
-        world:add(c, 14,16,10,10)
+
+        world:add({'a'}, 0,0,10,10)
+        local b = world:add({'b'}, 4,6,10,10)
+        world:add({'c'}, 14,16,10,10)
         assert.same(#world:check(b), 1)
 
       end)
 
       describe('when next l,t are passed', function()
         it('still handles intersections as before', function()
-          local world, a, b = bump.newWorld(), {'a'}, {'b'}
 
-          world:add(a, 0,0, 2,2)
-          world:add(b, 1,1, 2,2)
+
+          world:add({'a'}, 0,0, 2,2)
+          local b = world:add({'b'}, 1,1, 2,2)
           assert.same(#world:check(b, 1, 1), 1)
         end)
 
         it('detects and tags tunneling correctly', function()
-          local world, a, b = bump.newWorld(), {'a'}, {'b'}
 
-          world:add(a,  1,0, 2,1)
-          world:add(b, -5,0, 4,1)
+
+          world:add({'a'},  1,0, 2,1)
+          local b = world:add({'b'}, -5,0, 4,1)
           assert.same(#world:check(b, 5, 0), 1)
         end)
 
         it('detects the case where an object was touching another without intersecting, and then penetrates', function()
-          local world, a, b = bump.newWorld(), {'a'}, {'b'}
-
-          world:add(a, 32,50,20,20)
-          world:add(b, 0,0,32,100)
+          local a = world:add({'a'}, 32,50,20,20)
+          world:add({'b'}, 0,0,32,100)
 
           assert.same(#world:check(a, 30, 50), 1)
         end)
 
         it('returns a list of collisions sorted by ti', function()
-          local world, a, b, c, d = bump.newWorld(), {'a'}, {'b'}, {'c'}, {'d'}
+          local a = world:add({'a'}, 110,0, 10,10)
+          world:add({'b'}, 70,0, 10,10)
+          world:add({'c'}, 50,0, 10,10)
+          world:add({'d'}, 90,0, 10,10)
 
-          world:add(a, 110,0, 10,10)
-          world:add(b, 70,0, 10,10)
-          world:add(c, 50,0, 10,10)
-          world:add(d, 90,0, 10,10)
           local col = world:check(a, 10, 0)
 
           assert.same(collect(col, 'ti'), {0.1, 0.3, 0.5})
@@ -160,14 +150,13 @@ describe('World', function()
       end) -- when next l,t are passed
 
       describe('options', function()
-        local world, a, b, c, d
+        local a, b, c, d
 
         before_each(function()
-          world, a, b, c, d = bump.newWorld(), {'a'}, {'b'}, {'c'}, {'d'}
-          world:add(a, 110,0, 10,10)
-          world:add(b, 70,0, 10,10)
-          world:add(c, 50,0, 10,10)
-          world:add(d, 90,0, 10,10)
+          a = world:add({'a'}, 110,0, 10,10)
+          b = world:add({'b'}, 70,0, 10,10)
+          c = world:add({'c'}, 50,0, 10,10)
+          d = world:add({'d'}, 90,0, 10,10)
         end)
 
         describe('the filter param', function()
@@ -184,22 +173,18 @@ describe('World', function()
 
   describe(':remove', function()
     it('throws an error if the item does not exist', function()
-      local world = bump.newWorld()
       assert.error(function() world:remove({}) end)
     end)
     it('makes the item disappear from the world', function()
-      local world, a, b = bump.newWorld(), {}, {}
-
-      world:add(a, 0,0, 10,10)
-      world:add(b, 5,0, 1,1)
+      local a = world:add({'a'}, 0,0, 10,10)
+      local b = world:add({'b'}, 5,0, 1,1)
       assert.same(#world:check(b), 1)
       world:remove(a)
       assert.same(world:check(b), {})
     end)
     it('marks empty cells & rows for deallocation', function()
-      local world, a, b = bump.newWorld(), {}, {}
-      world:add(a, 0,0, 10, 10)
-      world:add(b, 200,200, 10,10)
+      world:add({'a'}, 0,0, 10, 10)
+      local b = world:add({'b'}, 200,200, 10,10)
       assert.same(world:countCells(), 2)
       world:remove(b)
       assert.same(world:countCells(), 2)
@@ -210,35 +195,32 @@ describe('World', function()
 
   describe(':toCell', function()
     it('returns the coordinates of the cell containing a point', function()
-      local w = bump.newWorld()
-      assert.same({w:toCell(0,0)}, {1,1})
-      assert.same({w:toCell(63.9,63.9)}, {1,1})
-      assert.same({w:toCell(64,64)}, {2,2})
-      assert.same({w:toCell(-1,-1)}, {0,0})
+      assert.same({world:toCell(0,0)}, {1,1})
+      assert.same({world:toCell(63.9,63.9)}, {1,1})
+      assert.same({world:toCell(64,64)}, {2,2})
+      assert.same({world:toCell(-1,-1)}, {0,0})
     end)
   end)
 
   describe(':toWorld', function()
     it('returns the world left,top corner of the given cell', function()
-      local w = bump.newWorld()
-      assert.same({w:toWorld(1,1)}, {0,0})
-      assert.same({w:toWorld(2,2)}, {64,64})
-      assert.same({w:toWorld(-1,1)}, {-128,0})
+      assert.same({world:toWorld(1,1)}, {0,0})
+      assert.same({world:toWorld(2,2)}, {64,64})
+      assert.same({world:toWorld(-1,1)}, {-128,0})
     end)
   end)
 
   describe(':queryRect', function()
     it('returns nothing when the world is empty', function()
-      assert.same(bump.newWorld():queryRect(0,0,1,1), {})
+      assert.same(world:queryRect(0,0,1,1), {})
     end)
     describe('when the world has items', function()
-      local world, a, b, c, d
+      local a, b, c, d
       before_each(function()
-        world, a, b, c, d = bump.newWorld(), {'a'}, {'b'}, {'c'}, {'d'}
-        world:add(a, 10,0, 10,10)
-        world:add(b, 70,0, 10,10)
-        world:add(c, 50,0, 10,10)
-        world:add(d, 90,0, 10,10)
+        a = world:add({'a'}, 10,0, 10,10)
+        b = world:add({'b'}, 70,0, 10,10)
+        c = world:add({'c'}, 50,0, 10,10)
+        d = world:add({'d'}, 90,0, 10,10)
       end)
       local sorted = function(tbl)
         table.sort(tbl, function(a,b) return a[1] < b[1] end)
@@ -260,15 +242,14 @@ describe('World', function()
 
   describe(':queryPoint', function()
     it('returns nothing when the world is empty', function()
-      assert.same(bump.newWorld():queryPoint(0,0), {})
+      assert.same(world:queryPoint(0,0), {})
     end)
     describe('when the world has items', function()
-      local world, a, b, c, d
+      local a, b, c
       before_each(function()
-        world, a, b, c, d = bump.newWorld(), {'a'}, {'b'}, {'c'}, {'d'}
-        world:add(a, 10,0, 10,10)
-        world:add(b, 15,0, 10,10)
-        world:add(c, 20,0, 10,10)
+        a = world:add({'a'}, 10,0, 10,10)
+        b = world:add({'b'}, 15,0, 10,10)
+        c = world:add({'c'}, 20,0, 10,10)
       end)
       local sorted = function(tbl)
         table.sort(tbl, function(a,b) return a[1] < b[1] end)
@@ -298,13 +279,12 @@ describe('World', function()
 
   describe(':querySegment', function()
     it('returns nothing when the world is empty', function()
-      assert.same(bump.newWorld():querySegment(0,0,1,1), {})
+      assert.same(world:querySegment(0,0,1,1), {})
     end)
 
     it('does not touch borders', function()
-      local world, a, b, c, d = bump.newWorld(), {'a'}, {'b'}, {'c'}, {'d'}
-      world:add(a, 10,0, 5,5)
-      world:add(c, 20,0, 5,5)
+      world:add({'a'}, 10,0, 5,5)
+      world:add({'c'}, 20,0, 5,5)
 
       assert.same(world:querySegment(0,5,  10,0),  {})
       assert.same(world:querySegment(15,5, 20,0),  {})
@@ -312,12 +292,11 @@ describe('World', function()
     end)
 
     describe("when the world has items", function()
-      local world, a, b, c, d
+      local a, b, c, d
       before_each(function()
-        world, a, b, c, d = bump.newWorld(), {'a'}, {'b'}, {'c'}, {'d'}
-        world:add(a,  5,0, 5,10)
-        world:add(b, 15,0, 5,10)
-        world:add(c, 25,0, 5,10)
+        a = world:add({'a'},  5,0, 5,10)
+        b = world:add({'b'}, 15,0, 5,10)
+        c = world:add({'c'}, 25,0, 5,10)
       end)
 
       it('returns the items touched by the segment, sorted by touch order', function()
@@ -355,13 +334,13 @@ describe('World', function()
   describe(":hasItem", function()
     it('returns wether the world has an item', function()
       local item = {}
-      local world = bump.newWorld()
+
       assert.is_false(world:hasItem(item))
       world:add(item, 0,0,1,1)
       assert.is_true(world:hasItem(item))
     end)
     it('does not throw errors with non-tables or nil', function()
-      local world = bump.newWorld()
+
       assert.is_false(world:hasItem(false))
       assert.is_false(world:hasItem(1))
       assert.is_false(world:hasItem("hello"))
