eb139e3610134e1a11a618ae82b724f709f152cd
kikito <kikito@gmail.com>
2014-12-17 10:36:24 +0100

    update version number

--- bump.lua
+++ bump.lua
@@ -1,5 +1,5 @@
 local bump = {
-  _VERSION     = 'bump v3.0.1',
+  _VERSION     = 'bump v3.1.0',
   _URL         = 'https://github.com/kikito/bump.lua',
   _DESCRIPTION = 'A collision detection library for Lua',
   _LICENSE     = [[
